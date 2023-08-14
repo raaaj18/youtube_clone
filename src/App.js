0539758c26c003042146c39c1543803f4c0fe8cc
raaaj18 <ritiksah2918@gmail.com>
2023-08-14 20:16:57 +0530

    updated components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
-function App() {
+
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import LeftNav from "./components/LeftNav";
+import LeftNavItem from "./components/LeftNavItem"
+import SearchResult from "./components/SearchResult"
+import SearchResultVC from "./components/SearchResultVC"
+import SuggestionVs from "./components/SuggestionVC"
+import VideoCard from "./components/VideoCard"
+import VideoDetails from "./components/VideoDetails"
+import { AppContext } from './context/contextApi';
+
+const App = () => {                                                                 //exact takes us to the main route page: onload route
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <AppContext>
+        <BrowserRouter>
+          <div className="flex flex-col h-full">
+            <Header/>
+              <Routes>
+                <Route path='/' exact element={<Feed />}/>                                   
+                <Route path='/searchResult/:searchQuery' element={<SearchResult />}/>
+                <Route path='/video/:id' element={<VideoDetails />}/>
+              
+              </Routes>
+          </div>
+      </BrowserRouter>
+    </AppContext>
+  );   
+};
 
 export default App;
